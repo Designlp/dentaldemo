0009b5f17383ff021a08bc5d23b57fb99eafb4c9
Diego <dmercadoff@gmail.com>
2024-03-17 21:17:41 -0400

    bd actualizada

--- include/config.php
+++ include/config.php
@@ -1,11 +1,11 @@
 <?php
-defined('server') ? null : define("server", "u583014169_dentaldemo");
+defined('server') ? null : define("server", "localhost");
 defined('user') ? null : define ("user", "u583014169_dentaldemo") ;
 defined('pass') ? null : define("pass","gtaV19921963");
-defined('database_name') ? null : define("database_name", "db_dentalclinic") ;
+defined('database_name') ? null : define("database_name", "u583014169_dentaldemo") ;
  
 // $web_root =  "https://mcguyverja.com/invoice/"; 
-$web_root ="http://localhost/dentalclinic/";
+$web_root ="https://laportada.online/";
 
 
 define ('web_root' , $web_root); 
