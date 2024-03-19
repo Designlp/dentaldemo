6d2e7b8c020877b59d5025c3f990bf242d372576
Diego <dmercadoff@gmail.com>
2024-03-19 15:14:00 -0400

    visual

--- sis/login.php
+++ sis/login.php
@@ -17,9 +17,6 @@ if(isset($_SESSION['ADMIN_USERID'])){
 
           color: #fff;
     }
-
-    /* config.css */
-
  </style>
 <!DOCTYPE html>
 <html lang="en">
@@ -49,58 +46,52 @@ if(isset($_SESSION['ADMIN_USERID'])){
   }
 </style>
 
-<body class="">
-
-  <div class="container">
 
 
-    <div class="row justify-content-center">
+<body class="align">
 
-      <div class="col-xl-10 col-lg-12 col-md-9">
+  <div class="grid">
+  <div style="font-size: 12px"> <?php check_message();?></div> 
+    <form method="POST" class="form login" action="process.php">
 
-        <div class="card o-hidden border-0 shadow-lg my-5">
-          <div class="card-body p-0">
-
-            <div class="row">
-              <div class="col-lg-6 d-none d-lg-block login-image-stretch">
-                <img src="dist/img/logoo.png">
-              </div>
-              <div class="col-lg-6">
-                <div class="p-5">
-                  <div class="text-center">
-                    <h1 class="h4 text-gray-900 mb-4">Login!</h1>
-                    <div style="font-size: 12px"> <?php check_message();?></div> 
-                  </div>
-                  <form class="user" method="POST" action="process.php">
-                    <div class="form-group">
-                      <input type="input" class="form-control form-control-user" id="user_email"  name="user_email" aria-describedby="emailHelp" placeholder="Enter Username...">
-                    </div>
-                    <div class="form-group">
-                      <input type="password" class="form-control form-control-user" id="user_pass" name="user_pass"  placeholder="Password">
-                    </div> 
-                    <button type="submit" name="btnLogin" class="btn btn-primary btn-user btn-block">
-                      Login
-                    </button>
-                    <hr>
-                     
-                  </form> 
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-
+      <div class="form__field">
+        <label for="login__username"><svg class="icon">
+            <use xlink:href="#icon-user"></use>
+          </svg><span class="hidden">Username</span></label>
+        <input autocomplete="username" id="user_email" type="text" name="user_email" aria-describedby="emailHelp" class="form__input" placeholder="Username" required>
       </div>
 
-    </div>
+      <div class="form__field">
+        <label for="login__password"><svg class="icon">
+            <use xlink:href="#icon-lock"></use>
+          </svg><span class="hidden">Password</span></label>
+        <input id="user_pass" name="user_pass" type="password" class="form__input" placeholder="Password" required>
+      </div>
+
+      <div class="form__field">
+        <input type="submit" name="btnLogin"  value="Sign In">
+      </div>
+
+    </form>
+
+    <p class="text--center">Not a member? <a href="#">Sign up now</a> <svg class="icon">
+        <use xlink:href="#icon-arrow-right"></use>
+      </svg></p>
 
   </div>
 
-
-
- 
+  <svg xmlns="http://www.w3.org/2000/svg" class="icons">
+    <symbol id="icon-arrow-right" viewBox="0 0 1792 1792">
+      <path d="M1600 960q0 54-37 91l-651 651q-39 37-91 37-51 0-90-37l-75-75q-38-38-38-91t38-91l293-293H245q-52 0-84.5-37.5T128 1024V896q0-53 32.5-90.5T245 768h704L656 474q-38-36-38-90t38-90l75-75q38-38 90-38 53 0 91 38l651 651q37 35 37 90z" />
+    </symbol>
+    <symbol id="icon-lock" viewBox="0 0 1792 1792">
+      <path d="M640 768h512V576q0-106-75-181t-181-75-181 75-75 181v192zm832 96v576q0 40-28 68t-68 28H416q-40 0-68-28t-28-68V864q0-40 28-68t68-28h32V576q0-184 132-316t316-132 316 132 132 316v192h32q40 0 68 28t28 68z" />
+    </symbol>
+    <symbol id="icon-user" viewBox="0 0 1792 1792">
+      <path d="M1600 1405q0 120-73 189.5t-194 69.5H459q-121 0-194-69.5T192 1405q0-53 3.5-103.5t14-109T236 1084t43-97.5 62-81 85.5-53.5T538 832q9 0 42 21.5t74.5 48 108 48T896 971t133.5-21.5 108-48 74.5-48 42-21.5q61 0 111.5 20t85.5 53.5 62 81 43 97.5 26.5 108.5 14 109 3.5 103.5zm-320-893q0 159-112.5 271.5T896 896 624.5 783.5 512 512t112.5-271.5T896 128t271.5 112.5T1280 512z" />
+    </symbol>
+  </svg>
 
 </body>
-
 
 </html>
