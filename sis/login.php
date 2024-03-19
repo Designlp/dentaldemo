640aa6ca5d63d71e72b5d0a52e22f0b826d82f9d
Diego <dmercadoff@gmail.com>
2024-03-19 15:11:12 -0400

    visual

--- sis/login.php
+++ sis/login.php
@@ -294,7 +294,58 @@ p {
   }
 </style>
 
+<body class="">
 
+  <div class="container">
+
+
+    <div class="row justify-content-center">
+
+      <div class="col-xl-10 col-lg-12 col-md-9">
+
+        <div class="card o-hidden border-0 shadow-lg my-5">
+          <div class="card-body p-0">
+
+            <div class="row">
+              <div class="col-lg-6 d-none d-lg-block login-image-stretch">
+                <img src="dist/img/logoo.png">
+              </div>
+              <div class="col-lg-6">
+                <div class="p-5">
+                  <div class="text-center">
+                    <h1 class="h4 text-gray-900 mb-4">Login!</h1>
+                    <div style="font-size: 12px"> <?php check_message();?></div> 
+                  </div>
+                  <form class="user" method="POST" action="process.php">
+                    <div class="form-group">
+                      <input type="input" class="form-control form-control-user" id="user_email"  name="user_email" aria-describedby="emailHelp" placeholder="Enter Username...">
+                    </div>
+                    <div class="form-group">
+                      <input type="password" class="form-control form-control-user" id="user_pass" name="user_pass"  placeholder="Password">
+                    </div> 
+                    <button type="submit" name="btnLogin" class="btn btn-primary btn-user btn-block">
+                      Login
+                    </button>
+                    <hr>
+                     
+                  </form> 
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+    </div>
+
+  </div>
+
+
+
+ 
+
+</body>
 
 
 <body class="align">
